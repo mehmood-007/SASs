e729db1e81b3ea72eb9751b1a82c4dbbd2e8eb11
mehmood-007 <mehmood.saleem108@gmail.com>
2021-02-18 10:00:23 +0100

    config alert rules

--- SASs/api.py
+++ SASs/api.py
@@ -1,29 +1,42 @@
+import logging
 import flask
 from flask import request, jsonify, abort
+import json
+import os
+
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Prom web API</h1>
-<p>Prom web service</p>'''
-
-# A route to return all of the available entries in our catalog.
+    return '''<h1>API service</h1>
+<p>Prometheus API server</p>'''
 
 @app.route('/api/v1/alerts', methods=['POST'])
 def create_task():
-    if not request.json: 
-        print(request)
+    if not request.json:
+        logging.error('Cannot recognise json')
         abort(400)
-#    task = {
-#        'id': tasks[-1]['id'] + 1,
-#        'title': request.json['title'],
-#        'description': request.json.get('description', ""),
-#        'done': False
-#    }
-#    tasks.append(task)
-    print(request.json)
-    return jsonify(books), 201
+    if request.json[0]['labels']['alertname'] == 'DefaultRequestWeb':
+        logging.info('Default Alerts')
+        stream = os.popen('docker service scale stack_web-Service=1')
+        output = stream.read()
+        stream = os.popen('docker service scale stack_cadvisor=1')
+        output = stream.read()
+        logging.info(output)
 
-app.run()
+    elif request.json[0]['labels']['alertname'] == 'CPUHighAlert':
+        stream = os.popen('docker service scale stack_web-Service=2')
+        output = stream.read()
+        stream = os.popen('docker service scale stack_cadvisor=2')
+        output = stream.read()
+        logging.info('High Load')
+        logging.info(output)
+
+    return jsonify(request.json), 201
+
+app.run(host="localhost", port=5050, debug=True)
